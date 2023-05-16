adf2926d8635f8f919a4c887a5fae471a7e2fb7f
kaustubhbhavsar <bhavsar.kaustubh@gmail.com>
2023-05-16 17:19:24 +0530

    updated file

--- config/config.py
+++ config/config.py
@@ -7,3 +7,9 @@ BASE_DIR = Path(os.path.abspath(__file__)).parent.parent.absolute()
 CONFIG_DIR = Path(BASE_DIR, "config")
 DATA_DIR = Path(BASE_DIR, "data")
 MODEL_DIR = Path(BASE_DIR, "model")
+ 
+
+# Hopsworks API Key
+# this key was used locally, which has been deleted when the code was made public
+# alternatively, one can also use api key file to login
+HOPSWORKS_API_KEY = 'xxxxx' 
